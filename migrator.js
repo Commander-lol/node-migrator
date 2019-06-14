dae04ba7a94b54a540488bbc4a6ed4aeced05ba9
Louis Capitanchik <contact@louiscap.co>
2019-06-14 17:33:31 +0100

    Set up linting

--- migrator.js
+++ migrator.js
@@ -22,13 +22,13 @@ module.exports = class Migrator {
 		const { rows } = await this._client.query('select "name" from _hnt_mgr')
 		const migrations = await migrationDir.listAsync()
 		let hasExecuted = false
-	
+
 		for (const migrationName of migrations) {
 			const missing = !rows.find(row => row.name === migrationName)
 			if (missing) {
 				const content = await fs.readAsync(migrationDir.path(migrationName, 'up.sql'))
 				await this._client.query('BEGIN')
-				this.log("---")
+				this.log('---')
 				this.log(`Migrating ${ migrationName }`)
 				try {
 					await this._client.query(content)
@@ -36,33 +36,33 @@ module.exports = class Migrator {
 					await this._client.query('COMMIT')
 					hasExecuted = true
 					this.log(`Successfully migrated ${ migrationName }`)
-				} catch(e) {
+				} catch (e) {
 					await this._client.query('ROLLBACK')
 					this.error('Failed to migrate %s', migrationName)
 					throw e
 				}
-				this.log("---")
+				this.log('---')
 			}
 		}
-	
+
 		if (!hasExecuted) {
-			this.log("No migrations to run")
+			this.log('No migrations to run')
 		}
 	}
-	
+
 	async rollback(path = this._path) {
 		const migrationDir = fs.dir(path)
 		await this._client.query('CREATE TABLE IF NOT EXISTS _hnt_mgr ( "name" TEXT PRIMARY KEY UNIQUE, "at" timestamptz NOT NULL default now() )')
 		const { rows } = await this._client.query('select "name" from _hnt_mgr ORDER BY "at" DESC')
 		const migrations = await migrationDir.listAsync()
 		let hasExecuted = false
-	
+
 		for (const row of rows) {
 			const migrationName = migrations.find(migrationName => row.name === migrationName)
 			if (migrationName != null) {
 				const content = await fs.readAsync(migrationDir.path(migrationName, 'down.sql'))
 				await this._client.query('BEGIN')
-				this.log("---")
+				this.log('---')
 				this.log(`Rolling back ${ migrationName }`)
 				try {
 					await this._client.query(content)
@@ -70,17 +70,17 @@ module.exports = class Migrator {
 					await this._client.query('COMMIT')
 					hasExecuted = true
 					this.log(`Successfully rolled back ${ migrationName }`)
-				} catch(e) {
+				} catch (e) {
 					await this._client.query('ROLLBACK')
 					this.error('Failed to roll back %s', migrationName)
 					throw e
 				}
-				this.log("---")
+				this.log('---')
 			}
 		}
-	
+
 		if (!hasExecuted) {
-			this.log("No migrations to roll back")
+			this.log('No migrations to roll back')
 		}
 	}
 
